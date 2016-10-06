3e62a6d735f917e6cf375635ec87b0504f0f5811
ChrisPeleti <chrisyred@gmail.com>
2016-10-06 15:13:13 -0400

    RIP TIM :'c

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 69, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Blue", "Rowdy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 5, "F", "Black", "Kind", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 20, "M", "Albino", "Triggered", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Brown", "Calm", 10);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 69, "M", "Brown", "Mean", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "F", "Black", "Calm", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 100, "M", "Brown", "Violent", 1);
